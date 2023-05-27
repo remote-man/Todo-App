5d1ae630a1f70fde1050c0897deb1c52d08edb7b
Max <mk.vempati1311@gmail.com>
2023-05-27 10:38:03 +1000

    Implemented the Add button on the GUI.

--- venv/ToDoAppGUI.py
+++ venv/ToDoAppGUI.py
@@ -2,10 +2,25 @@ import main_app_functions
 import PySimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter a to-do")
+input_box = sg.InputText(tooltip="Enter a to-do", key="todo")
+import PySimpleGUI as sg
 add_button = sg.Button("Add")
 
-window = sg.Window('My To-do App', layout=[[label], [input_box, add_button]])
-window.read()
-print("Hello")
+window = sg.Window('My To-do App',
+                   layout=[[label], [input_box, add_button]],
+                   font=('Helvetica', 14))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = main_app_functions.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            main_app_functions.write_todos(todos)
+        case sg.WIN_CLOSED
+            break
+
 window.close()
--- venv/main_app_functions.py
+++ venv/main_app_functions.py
@@ -1,14 +1,14 @@
 FILEPATH = "todos.txt"
 
 
-def get_todos(filepath="FILEPATH"):
+def get_todos(filepath=FILEPATH):
     """Raad text file amd return a list of to-do items."""
     with open(filepath, 'r') as file:
         todos = file.readlines()  # no need to use.close()
     return todos
 
 
-def write_todos(todos_arg, filepath="FILEPATH"): # AS the filepath has been included in this function, it has beem
+def write_todos(todos_arg, filepath=FILEPATH): # AS the filepath has been included in this function, it has beem
     """Open the text file amd write the list of to-do items given as arguemnets. """
     with open(filepath, 'w') as file:              # removed from the call below.
         file.writelines(todos_arg)
