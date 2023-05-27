54ab2c86ced739dcf6d7dfbb36480a4edb35dd36
Max <mk.vempati1311@gmail.com>
2023-05-27 13:46:22 +1000

    Implemented the Edit button and textbox.

--- venv/ToDoAppGUI.py
+++ venv/ToDoAppGUI.py
@@ -5,9 +5,13 @@ label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter a to-do", key="todo")
 import PySimpleGUI as sg
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=main_app_functions.get_todos(), key='todos',
+                      enable_events=True, size=[45, 10])
+edit_button = sg.Button("Edit")
 
-window = sg.Window('My To-do App',
-                   layout=[[label], [input_box, add_button]],
+window = sg.Window('My To-do App',  #each list is a new line
+                   layout=[[label], [input_box, add_button],
+                   [list_box, edit_button]],
                    font=('Helvetica', 14))
 
 while True:
@@ -20,7 +24,18 @@ while True:
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             main_app_functions.write_todos(todos)
-        case sg.WIN_CLOSED
+            window['todos'].update(values=todos)
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+            todos = main_app_functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            main_app_functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
+        case sg.WIN_CLOSED:
             break
 
 window.close()
