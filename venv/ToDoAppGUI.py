2be97c5742e29cf54bdc1802b441630b6795d4ed
Max <mk.vempati1311@gmail.com>
2023-05-30 07:31:54 +1000

    Implemented the Complete and Exit button.

--- venv/ToDoAppGUI.py
+++ venv/ToDoAppGUI.py
@@ -8,11 +8,16 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=main_app_functions.get_todos(), key='todos',
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
+
+
+layout=[[label], [input_box, add_button],
+                   [list_box, edit_button, complete_button],
+                    [exit_button]]
 
 window = sg.Window('My To-do App',  #each list is a new line
-                   layout=[[label], [input_box, add_button],
-                   [list_box, edit_button]],
-                   font=('Helvetica', 14))
+                   layout= layout, font=('Helvetica', 14))
 
 while True:
     event, values = window.read()
@@ -25,6 +30,7 @@ while True:
             todos.append(new_todo)
             main_app_functions.write_todos(todos)
             window['todos'].update(values=todos)
+
         case "Edit":
             todo_to_edit = values['todos'][0]
             new_todo = values['todo']
@@ -33,6 +39,16 @@ while True:
             todos[index] = new_todo
             main_app_functions.write_todos(todos)
             window['todos'].update(values=todos)
+
+        case "Complete":
+            todos_to_complete = values['todos'][0]
+            todos = main_app_functions.get_todos()
+            todos.remove(todos_to_complete)
+            main_app_functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value="")
+        case "Exit":
+            break
         case 'todos':
             window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
