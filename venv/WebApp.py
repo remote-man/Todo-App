fd66210276cd62b85698d091e7cbbd7497392c2d
Max <mk.vempati1311@gmail.com>
2023-06-03 17:52:24 +1000

    App 1 final touches

---
+++ venv/WebApp.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import main_app_functions as functions
+
+todos = functions.get_todos()
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
+st.title("My ToDo App")
+st.subheader("This is my ToDo App")
+st.write("This app is to increase your productivity")
+
+for item in (todos):
+    st.checkbox(item)
+
+
+st.text_input(label="", placeholder="Enter item here", on_change=add_todo,
+              key='new_todo')
+
+st.session_state
